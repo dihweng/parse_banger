b18927a353a626ca3a7c8f11cfcb79d78bab231b
Dihweng Albert <dihwengalbert@gmail.com>
2017-05-23 17:58:35 +0100

    final config
    configuration

--- configs/config.js
+++ configs/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    AppName: "WeMunize",
-    APP_ID: process.env.APP_ID || 'myAppId',
-    MASTER_KEY: process.env.MASTER_KEY || 'myMasterKey',
-    SERVER_URL: process.env.SERVER_URL || 'https://wemunize-dretnan.c9users.io/parse',
-    DASHBOARD_USER: process.env.DASHBOARD_USER || "retnan",
-    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "yahweh",
+    AppName: "RadioBanga",
+    APP_ID: process.env.APP_ID || 'nhub_banger',
+    MASTER_KEY: process.env.MASTER_KEY || 'bangerkey',
+    SERVER_URL: process.env.SERVER_URL || 'http://nhubbanger.herokuapp.com/parse',
+    DASHBOARD_USER: process.env.DASHBOARD_USER || "dihweng",
+    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "dihweng",
 }
